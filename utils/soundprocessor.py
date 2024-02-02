53e259e068d2c5b1b74bd5db3aba7b078838638b
bs-jes <esjeoung@bs-soft.co.kr>
2024-02-02 09:25:57 +0900

    :sparkles: long wav file support

--- utils/soundprocessor.py
+++ utils/soundprocessor.py
@@ -1,12 +1,21 @@
 import torch, librosa
 
-def stt(filepath: str, processors: dict, model, thres = 0.3, sr = 16000):
-    wav_data = librosa.load(filepath, sr=sr)[0]
+def predict(processors: dict, model, sr, wav_data):
     tmp = processors['processor'](wav_data, sampling_rate = sr, return_tensors='pt')
     input_values = tmp.input_features.to('cuda')
     with torch.no_grad():
-        # generated_ids = model.generate(input_values, forced_decoder_ids=processors['forced_decoder_ids'])
         generated_ids = model.generate(input_values)
     pred = processors['processor'].batch_decode(generated_ids, skip_special_tokens=True)[0]
     print(pred)
     return pred
+
+def stt(filepath: str, processors: dict, model, thres = 0.3, sr = 16000):
+    wav_data = librosa.load(filepath, sr=sr)[0]
+    sentence = ''
+    if len(wav_data) / sr > 30:
+        for i in range(0, len(wav_data), sr*30):
+            pred = predict(processors, model, sr, wav_data[i:i+sr*30])
+            sentence = sentence + pred
+    else:
+        sentence = predict(processors, model, sr, wav_data)
+    return sentence
